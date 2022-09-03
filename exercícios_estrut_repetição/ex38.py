12084ed4a2007a67e91e4cbf5cb918e32afd0462
Mariana Marinho <mariana.marinhoa@gmail.com>
2022-09-03 00:09:04 -0300

    exercícios otimizados: 38

--- exercícios_estrut_repetição/ex38.py
+++ exercícios_estrut_repetição/ex38.py
@@ -4,7 +4,7 @@ Calcular termo pitagórico a, b, c para a+b+c=1000
 for a in range(0, 1001):
     for b in range(0, 1001):
         for c in range(0, 1001):
+            if a + b + c == 1000:
+                break
             if a**2+b**2 == c**2 and a != b != c:
                 print(f'{a}² + {b}² = {c}²')
-            if a + b + c == 1000:
-                continue
